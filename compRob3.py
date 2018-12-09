17febb393c07d344930e72a3c290446b6c5fbc51
jackchiu7 <jc2001@scarletmail.rutgers.edu>
2018-12-08 20:35:40 -0500

    asd

--- compRob3.py
+++ compRob3.py
@@ -7,7 +7,9 @@ Created on Sat Dec  8 14:32:39 2018
 
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 from numpy.random import uniform 
+from numpy.random import randn
 from filterpy.stats import plot_gaussian_pdf
 import math
 from shapely import geometry
@@ -24,19 +26,19 @@ import random as rnd
 worldBounds = [[-5,1],[-3,-2]]    #bounds of the world/room
 Pi = math.pi
 obstacles = []
+particles = []
+nodes = [] #tuple of (node, weight)
+
 
 class Node():
     pos = [0,0,0]
     x = 0
     y = 0
     r = 0
-    connected = []
-    parent = None
-    g = 0
     
     def __str__(self):
         temp = 'Position: ' + '('+ str(self.x)
-        temp += ', ' + str(self.y) + ')'
+        temp += ', ' + str(self.y) + ')' + ' Rotation: ' + str(self.r)
         return temp
     def setPos(self,x,y): #set position of Node at x,y,z
         self.x = x
@@ -50,13 +52,13 @@ class Node():
         self.pos[2] = angle
         return self.pos
 
-def makeNode(): #generates a random node with random Quaternion/position
+def makeNode(posX, posY, rotation): #generates a random node with random Quaternion/position
     node = Node()
-    global worldBounds
-    bounds = worldBounds
-    posX = rnd.random()*(bounds[0][1]-bounds[0][0]) + bounds[0][0] 
-    posY = rnd.random()*(bounds[1][1]-bounds[1][0]) + bounds[1][0] 
-    rotation = rnd.random()*(2*Pi)-Pi
+#    global worldBounds
+#    bounds = worldBounds
+#    posX = rnd.random()*(bounds[0][1]-bounds[0][0]) + bounds[0][0] 
+#    posY = rnd.random()*(bounds[1][1]-bounds[1][0]) + bounds[1][0] 
+#    rotation = rnd.random()*(2*Pi)-Pi
     node.pos = [posX,posY, rotation]
     node.x = posX
     node.y = posY
@@ -139,6 +141,52 @@ def makeWorld(fileName,k):
         lines = lines[1::] 
 
 
+def createUniform(xRange, yRange, rRange, N):
+    particles = np.empty((N, 3))
+    particles[:, 0] = uniform(xRange[0],xRange[1], size=N)
+    particles[:, 1] = uniform(yRange[0],yRange[1], size=N)
+    particles[:, 2] = uniform(rRange[0],rRange[1], size=N)
+    particles[:, 2] %= 2 * Pi
+    particles[:, 2] -= Pi #to make it range -Pi to Pi
+    
+    return particles
+
+def createGaussian(meanVec, stdVec, N):
+    particles = np.empty((N, 3))
+    particles[:, 0] = meanVec[0] + (randn(N) * stdVec[0])
+    particles[:, 1] = meanVec[1] + (randn(N) * stdVec[1])
+    particles[:, 2] = meanVec[2] + (randn(N) * stdVec[2])
+    particles[:, 2] %= 2 * Pi
+    particles[:, 2] -= Pi
+    
+    return particles
+
+def predict(particles, u, std, dt=1.):
+    """ move according to control input u (heading change, velocity)
+    with noise Q (std heading change, std velocity)`"""
+
+    N = len(particles)
+    # update heading
+    particles[:, 2] += u[0] + (randn(N) * std[0])
+    particles[:, 2] %= 2 * Pi
+    particles[:, 2] -= Pi
+
+    # move in the (noisy) commanded direction
+    dist = (u[1] * dt) + (randn(N) * std[1])
+    particles[:, 0] += np.cos(particles[:, 2]) * dist
+    particles[:, 1] += np.sin(particles[:, 2]) * dist
+
+    return particles
+
+def update():
+    a = 1
+    
+def propagate():
+    a = 1
+    
+
+
+
 
 
 
@@ -146,13 +194,32 @@ def makeWorld(fileName,k):
 def main():
     
     makeWorld('grid1.txt',3)
+    a = createGaussian([0,0,0], [2,2,2] ,5)
+    
+#    if os.name == 'nt':
+#        clear = lambda: os.system('!CLS')
+    print '\033[H\033[J'
+#    else:
+#        clear = lambda: os.system('!clear')
+    
+    
+    
+    
+    
+    
+    plt.gcf().clear()
     
     graph = True
-    
     if graph:
         fig, ax = plt.subplots(1,1,figsize = (10,10))
+#        plt.gcf().clear()    
+        time.sleep(.01)
         drawWorld(ax)
-        print str(ax.axes)
+        ax.plot(rnd.uniform(-6, 4),rnd.uniform(-6, 4), 'ro', markersize = 5)
+        plt.show()
+#            clear()
+        time.sleep(.01)
+#        print str(ax.axes)
 
 
 
